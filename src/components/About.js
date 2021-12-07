a14377e993187dad8f76d9c1531e120e876d3f67
Nima sherpa <Toplouie78@gmail.com>
2021-12-07 12:03:27 -0500

    About page css lil done

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import "/Users/toplouie/Desktop/youtube-clone/src/components/About.css";
 
 export default class About extends Component {
 	render() {
 		return (
-			<div>
+			<div className="about-main">
 				<h1>Youtube-clone</h1>
 				<p>This will be the description place</p>
 
-				<h2>Members</h2>
-				<ul>
-					<li>Nima Sherpa</li>
-					<li>Charlie Moran</li>
-					<li>Deborah Campos</li>
-				</ul>
-				<h2>GitHub Links</h2>
-				<a href="https://github.com/toplouie1"> Nima GitHub</a>
+				<div className="about-contains">
+					<div className="members-about">
+						<h2>Members</h2>
+
+						<h5>Nima Sherpa</h5>
+						<div>
+							Hey my name is nima sherpa I m an aspiring software developer on
+							my way to change the world.{" "}
+						</div>
+						<h5>Charlie Moran</h5>
+						<div>charlie bio ...</div>
+
+						<h5>Deborah Campos</h5>
+						<div>deborah bio ...</div>
+					</div>
+					<div className="github-links">
+						<h2>GitHub Links</h2>
+						<a target="blank" href="https://github.com/toplouie1">
+							Nima GitHub
+						</a>
+					</div>
+				</div>
 			</div>
 		);
 	}
