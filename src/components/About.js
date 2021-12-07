1792b089ec43aafa6bb3011fc20b363c83d0f00d
Nima sherpa <Toplouie78@gmail.com>
2021-12-07 10:46:57 -0500

    About page

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Youtube-clone</h1>
+				<p>This will be the description place</p>
+
+				<h2>Members</h2>
+				<ul>
+					<li>Nima Sherpa</li>
+					<li>Charlie Moran</li>
+					<li>Deborah Campos</li>
+				</ul>
+				<h2>GitHub Links</h2>
+				<a href="https://github.com/toplouie1"> Nima GitHub</a>
+			</div>
+		);
+	}
+}
