624a607d96b21b8837cfa0e87ef313f3e4cc3ffb
Deborah Campos <631care@gmail.com>
2021-12-08 13:15:22 -0500

    fetch by search terms complete, videos render

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
+import Youtube from 'react-youtube';
+
 
 export default class Home extends Component {
+ constructor(){
+	 super()
+
+	 this.state = {
+		 searchValue: "",
+		 fetchdata: []
+
+	 }
+ }
+
+ setSearch=(e)=>{
+	 this.setState({
+		 searchValue: e.target.value
+
+	 })
+ }
+ fetchSearchVid=(e)=>{
+	 e.preventDefault();
+
+	 fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${this.state.searchValue}&type=video&key=AIzaSyAy4drePBpLUxVGLeDlqaOi3PMlu1mSmMU`)
+	 .then((res)=>{
+		 return res.json();
+	 }).then((data)=>{
+		 this.setState({
+			 fetchdata: data.items
+		 });
+	 });
+ }
+
+  
+ 
 	render() {
+		let videos = this.state.fetchdata.map((vid)=>{
+			return (
+				<Youtube videoId={vid.id.videoId} opts={{height: '200', width:'400'}} />
+				)
+		})
+		console.log(videos)
 		return (
 			<div>
-				<input
-					placeholder="Ariana granda "
-					type="text"
-					id="search"
-					name="search"
-				/>
-				<button type="submit">Search</button>
+				<form onSubmit={this.fetchSearchVid}>
+					<input onInput={this.setSearch}
+						placeholder="enter search here"
+						type="text"
+						id="search"
+						name="search"
+					/>
+					<button type="submit">Search</button>
+				</form>
 
-				<div class="video-container"></div>
+				<div id="display-container" className="video-container">
+					This is where the videos display
+					<div className="video">
+						{videos}
+					</div>
+				</div>
 			</div>
 		);
 	}
