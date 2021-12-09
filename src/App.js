42ac344d18c6fa319eb6e6a17b18e682d5879978
Nima sherpa <Toplouie78@gmail.com>
2021-12-09 14:17:08 -0500

    added the video Link

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import About from "./components/About";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import { Switch, Route } from "react-router-dom";
+import Video from "./components/Video";
 
 function App() {
 	return (
 		<div>
 			<Nav />
 			<Switch>
-				<Route exact path="/Home">
+				<Route exact path="/">
 					<Home />
 				</Route>
-				<Route exact path="/About">
+				<Route exact path="/Video/:id">
+					<Video />
+				</Route>
+				<Route path="/About">
 					<About />
 				</Route>
 				<Route path="/About">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import Youtube from "react-youtube";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
 	constructor() {
@@ -10,7 +11,7 @@ export default class Home extends Component {
 			searchValue: "",
 			fetchdata: [],
 			noResults: "No Search Results Yet! Please submit a search above!",
-			vidId: ""
+			vidId: "",
 		};
 	}
 
@@ -23,7 +24,7 @@ export default class Home extends Component {
 		e.preventDefault();
 
 		fetch(
-			`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${this.state.searchValue}&type=video&key=AIzaSyAy4drePBpLUxVGLeDlqaOi3PMlu1mSmMU`
+			`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${this.state.searchValue}&type=video&key=${process.env.REACT_APP_API_KEY}`
 		)
 			.then((res) => {
 				return res.json();
@@ -37,25 +38,26 @@ export default class Home extends Component {
 		console.log(process.env);
 	};
 
-	viewVideo=(e)=>{
+	viewVideo = (e) => {
 		this.setState({
-			vidId: e.id.videoId
+			vidId: e.id.videoId,
 		});
-	}
+	};
 
 	render() {
-		console.log(this.state.vidId)
+		console.log(this.state.vidId);
 		let videos = this.state.fetchdata.map((vid) => {
 			return (
 				<div>
 					<div className="vid">
-						<div value={vid.id.videoId} onClick={()=>this.viewVideo(vid)}>
-							<img src={vid.snippet.thumbnails.high.url} alt ={vid.snippet.title}/>
+						<div value={vid.id.videoId} onClick={() => this.viewVideo(vid)}>
+							<Link to="/Video/:id">
+								<img
+									src={vid.snippet.thumbnails.high.url}
+									alt={vid.snippet.title}
+								/>
+							</Link>
 						</div>
-						{/* <Youtube
-							// videoId={this.props.vidId}
-							opts={{ height: "200", width: "400" }}
-						/> */}
 					</div>
 					<div className="title">{vid.snippet.title}</div>
 				</div>
@@ -64,23 +66,23 @@ export default class Home extends Component {
 		return (
 			<div>
 				<div id="form-container">
-				<form onSubmit={this.fetchSearchVid}>
-					<span>
-					<input
-						className="search"
-						onInput={this.setSearch}
-						placeholder="Search..."
-						type="text"
-						id="search"
-						name="search"
-					/>
-					</span>
-					<span><button className="submit" type="submit">
-						Search
-					</button>
-					</span>
-				</form>
-
+					<form onSubmit={this.fetchSearchVid}>
+						<span>
+							<input
+								className="search"
+								onInput={this.setSearch}
+								placeholder="Search..."
+								type="text"
+								id="search"
+								name="search"
+							/>
+						</span>
+						<span>
+							<button className="submit" type="submit">
+								Search
+							</button>
+						</span>
+					</form>
 				</div>
 				<div id="display-container" className="video-container">
 					{/* <div id="no-results">{this.state.noResults}</div> */}
