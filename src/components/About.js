bb72af55df81f232695fd7d3953c6548cc622667
Nima sherpa <Toplouie78@gmail.com>
2021-12-10 20:52:40 -0500

    changes some css

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,10 @@ export default class About extends Component {
 		return (
 			<div className="about-main">
 				<h1>YouTube-clone</h1>
-
 				<p>
 					This is a Youtube-clone that is done by the member's below making use
 					of the Youtube Api and the react-youtube.
 				</p>
-
 				<div className="about-contains">
 					<div className="members-about">
 						<h2>Members</h2>
